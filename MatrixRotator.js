73bd9d60644444239d8f4dca3dfcd330c08a61d1
Nikki Y Kobayashi <nikz@MacBook-Pro-2.local>
2016-05-07 19:17:55 -1000

    Clock

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -13,15 +13,30 @@ var Direction = require("./Direction").Direction;
 
 function MatrixRotator(matrix){
   this.matrix = matrix;
-  
+  // console.log(this.matrix);
 };
 
 //                                         |-- Must be Direction.CW
 //                                         v        or Direction.CCW
 MatrixRotator.prototype.rotate = function(direction) {
   // do work here
-
   // must be a valid Direction, see Direction.js
-
-
+ var newArr = [];
+ if(direction===Direction.CW){
+ 	 for(var i=0; i<this.matrix.length; i++){
+ 	newArr.push([]);
+ 	for (var j = this.matrix.length - 1; j >= 0; j--) {
+ 		newArr[i].push(this.matrix[j][i]);
+ 	}
+ }
+ }else{
+ 	for(var k=0; k<this.matrix.length; k++){
+ 	newArr.push([]);
+ 	for (var m = 0; m < this.matrix.length; m++) {
+ 		newArr[k].unshift(this.matrix[m][k]);
+ 	}
+ }
+ }
+ console.log(newArr);
+ this.matrix=newArr;
 };
